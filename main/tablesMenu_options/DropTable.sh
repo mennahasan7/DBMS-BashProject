12ed5c790926708e746f7489ae4f28e72ad4b1d2
alaaIseif <alaaseif.3112@gmail.com>
2023-08-11 07:52:17 +0300

    drop table and list table scripts

--- main/tablesMenu_options/DropTable.sh
+++ main/tablesMenu_options/DropTable.sh
@@ -1,14 +1,21 @@
 #! /bin/bash
 
-read -p "Please Enter the Name of the Table " tbName
+ntables=`ls -F | grep * | wc -l`
 
+if [ $ntables -eq 0 ];then 
+    echo "No tables found"
+else 
+    ls -F | grep * 
+fi               
+read -p "Please Enter the Name of the Table " tableName
 
-if [ -f $tbname ]; then 
-         #metaTable=`echo $tbName"_meta"` 
-         rm $tbName
-         #rm $metaTable
-         echo " Table has been Deleted Successfully "
-else "This Table is Not Exist "
+metadatafile=md_$tableName
+if [ -f $tableName ]; then 
+         rm $tableName
+         rm $metadatafile
+         echo -e " Table $tableName has been Deleted Successfully! \U2705"
+         echo -e " Metadata $metadatafile has been Deleted Successfully! \U2705"
+else -e " This Table is Not Exist \U274C"
 fi
 
-. ../Tablesmenuu
+. ../../main/Tablesmenu.sh
--- main/tablesMenu_options/ListTables.sh
+++ main/tablesMenu_options/ListTables.sh
@@ -1,24 +1,38 @@
 #! /bin/bash
 
 ntables=`ls -F | grep * | wc -l`
-    
-if [ $ntables -eq 0 ];
-   then echo "No tables found"
-else ls -F | grep *
-fi
-. ../Tablesmenu.sh
 
-# wala cat for list specific table ?
-
-
-read -p "Enter table name you want to list : " tableName
-
-if [ -f $tableName ] ;then
-    cat $tableName
-
+if [ $ntables -eq 0 ];then 
+    echo "No tables found"
 else 
-       echo "Table with name $tableName Not exists"
-       ListTables.sh
-fi
-. ../Tablesmenu.sh
+    select choice in "List all tables" "List a specfic table" "Back to menu"
+        do
+            case $choice in
 
+            "List all tables" )        
+            ls -F | grep *            
+            bash ../../main/Tablesmenu.sh
+            ;;
+
+            "List a specfic table" )
+
+            read -p "Enter table name you want to list : " tableName
+
+            if [ -f $tableName ] ;then
+                cat $tableName
+
+            else 
+                echo "Table with name $tableName Not exists"
+                bash ../../main/tablesMenu_options/ListTables.sh
+            fi
+            bash ../../main/Tablesmenu.sh
+            ;;
+            "Back to menu" )
+            bash ../../main/Tablesmenu.sh
+            ;;
+
+            * )
+            echo -e "Invalid choice \U274C" ;;
+            esac
+        done
+fi
